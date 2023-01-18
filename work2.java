aa246c822c6b6de364ad2ba87e68a475f94a71da
Erixhamucaa1 <122046005+Erixhamucaa1@users.noreply.github.com>
2023-01-18 22:08:24 +0100

    Add files via upload

---
+++ work2.java
@@ -0,0 +1,9 @@
+public class work2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String kopjuteri = "ASUS" ;
+		System.out.println("The best brand of pc is " + kopjuteri);
+	}
+
+}
